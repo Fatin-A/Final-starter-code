3348a8f27fe021601790cef7e9eee92b28e3ce78
Fatin-A <ftnx0098@gmail.com>
2022-07-18 03:25:00 +0800

    B9

--- lib/edit_screen.dart
+++ lib/edit_screen.dart
@@ -19,68 +19,70 @@ class _EditScreenState extends State<EditScreen> {
   String title = '';
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: Container(),
-        centerTitle: true,
-        title: const Text('App Bar Title'),
-        actions: [
-          IconButton(
-              icon: const Icon(
-                Icons.check_circle,
-                size: 30,
-              ),
-              onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => HomeScreen()));
-              }),
-          IconButton(
-              icon: const Icon(
-                Icons.cancel_sharp,
-                size: 30,
-              ),
-              onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => HomeScreen()));
-              }),
-        ],
-      ),
-      body: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 10.0),
-        child: Column(
-          children: [
-            TextFormField(
-              controller: _titleController,
-              initialValue: null,
-              enabled: true,
-              decoration: const InputDecoration(
-                hintText: 'Type the title here',
-              ),
-              onChanged: (value) {
-                setState(() => title = value);
-              },
-            ),
-            const SizedBox(
-              height: 5,
-            ),
-            Expanded(
-              child: TextFormField(
-                  controller: _descriptionController,
-                  enabled: true,
-                  initialValue: null,
-                  maxLines: null,
-                  expands: true,
-                  decoration: const InputDecoration(
-                    hintText: 'Type the description',
-                  ),
-                  onChanged: (value) {
-                    setState(() => content = value);
-                  }),
-            ),
+  Widget build(BuildContext context) => WillPopScope(
+      onWillPop: () async {
+        return true;
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          leading: Container(),
+          centerTitle: true,
+          title: const Text('App Bar Title'),
+          actions: [
+            IconButton(
+                icon: const Icon(
+                  Icons.check_circle,
+                  size: 30,
+                ),
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => HomeScreen()));
+                }),
+            IconButton(
+                icon: const Icon(
+                  Icons.cancel_sharp,
+                  size: 30,
+                ),
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => HomeScreen()));
+                }),
           ],
         ),
-      ),
-    );
-  }
+        body: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 10.0),
+          child: Column(
+            children: [
+              TextFormField(
+                controller: _titleController,
+                initialValue: null,
+                enabled: true,
+                decoration: const InputDecoration(
+                  hintText: 'Type the title here',
+                ),
+                onChanged: (value) {
+                  setState(() => title = value);
+                },
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              Expanded(
+                child: TextFormField(
+                    controller: _descriptionController,
+                    enabled: true,
+                    initialValue: null,
+                    maxLines: null,
+                    expands: true,
+                    decoration: const InputDecoration(
+                      hintText: 'Type the description',
+                    ),
+                    onChanged: (value) {
+                      setState(() => content = value);
+                    }),
+              ),
+            ],
+          ),
+        ),
+      ));
 }
